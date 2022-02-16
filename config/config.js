8b5593cf859846eaaec9e74f811b36f525ac06e3
Ronald Amiquero <roamz.2017@gmail.com>
2022-02-16 18:47:12 -0500

    Init commit

---
+++ config/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const config = {
+  env: process.env.NODE_ENV || 'dev',
+  isProd: process.env.NODE_ENV === 'production',
+  port: process.env.PORT || 3000,
+  dbUrl: process.env.DATABASE_URL,
+}
+
+module.exports = { config };
